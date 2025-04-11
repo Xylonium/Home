b915d366f158dfa261a38f447a06920ceac94891
XylonStar <xylonstar@outlook.com>
2025-04-12 07:55:13 +0800

    🌈 style: 更新站点信息和链接，添加新图标

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, Compass, Music, Hotjar, CommentDots, RssSquare, NetworkWired, Book, Icicles, Leanpub, GlobeAmericas, Docker, PhotoVideo, CloudDownloadAlt, InternetExplorer, DiceD6, ArrowAltCircleDown, DotCircle, Sun, Code, BehanceSquare, Image ,Tv ,HandSpock } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -64,12 +64,29 @@ const siteLinksList = computed(() => {
 // 网站链接图标
 const siteIcon = {
   Blog,
-  Cloud,
-  CompactDisc,
   Compass,
+  Music,
+  Hotjar,
+  CommentDots,
+  RssSquare,
+  NetworkWired,
   Book,
-  Fire,
-  LaptopCode,
+  Icicles,
+  Leanpub,
+  GlobeAmericas,
+  Docker,
+  PhotoVideo,
+  CloudDownloadAlt,
+  InternetExplorer,
+  DiceD6,
+  ArrowAltCircleDown,
+  DotCircle,
+  Sun,
+  Code,
+  BehanceSquare,
+  Image,
+  Tv,
+  HandSpock,
 };
 
 // 链接跳转
